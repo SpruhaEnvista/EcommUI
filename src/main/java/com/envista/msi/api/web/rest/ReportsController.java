34790a57cd72fd82b5e898087782d3f2799f2472
sreenivaslua <sreenivaslua@envistacorp.com>
2017-02-23 16:57:05 +0530

    merge with uat

--- src/main/java/com/envista/msi/api/web/rest/ReportsController.java
+++ src/main/java/com/envista/msi/api/web/rest/ReportsController.java
@@ -4,6 +4,7 @@ import com.envista.msi.api.service.ReportsService;
 import com.envista.msi.api.web.rest.dto.UserProfileDto;
 import com.envista.msi.api.web.rest.dto.dashboard.DashboardsFilterCriteria;
 import com.envista.msi.api.web.rest.dto.reports.ReportResultsDto;
+import com.envista.msi.api.web.rest.dto.reports.ReportResultsUsersListDto;
 import org.json.JSONObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -32,14 +33,14 @@ public class ReportsController {
         return new ResponseEntity<List<ReportResultsDto>>(resultsList, HttpStatus.OK);
     }
     @RequestMapping(value = "/results/updateexpirydate", method = {RequestMethod.GET, RequestMethod.POST, RequestMethod.OPTIONS}, produces = {MediaType.APPLICATION_JSON_VALUE})
-    public ResponseEntity<ReportResultsDto> getReportResults(@RequestParam Long generatedRptId, @RequestParam String expiryDate){
-        ReportResultsDto reportResultsDto = reportsService.updateExpiryDate(generatedRptId, expiryDate );
+    public ResponseEntity<ReportResultsDto> updateExpiryDate(@RequestParam String generatedRptId, @RequestParam String expiryDate){
+        ReportResultsDto reportResultsDto = reportsService.updateExpiryDate(Long.parseLong(generatedRptId), expiryDate );
         return new ResponseEntity<ReportResultsDto>(reportResultsDto, HttpStatus.OK);
     }
-    @RequestMapping(value = "/userslist/{userId}", method = {RequestMethod.GET, RequestMethod.POST, RequestMethod.OPTIONS}, produces = {MediaType.APPLICATION_JSON_VALUE})
-    public ResponseEntity<List<ReportResultsDto>> getUsersList(@PathVariable String userId){
-        List<ReportResultsDto> resultsList = reportsService.getReportResults(Long.parseLong(userId));
-        return new ResponseEntity<List<ReportResultsDto>>(resultsList, HttpStatus.OK);
+    @RequestMapping(value = "/results/userslist", method = {RequestMethod.GET, RequestMethod.POST, RequestMethod.OPTIONS}, produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<List<ReportResultsUsersListDto>> getUsersList(){
+        List<ReportResultsUsersListDto> usersList = reportsService.getUsersList();
+        return new ResponseEntity<List<ReportResultsUsersListDto>>(usersList, HttpStatus.OK);
     }
     @RequestMapping(value = "/results/delete", method = {RequestMethod.GET, RequestMethod.POST, RequestMethod.OPTIONS}, produces = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<ReportResultsDto> deleteReportInResult(@RequestParam String generatedRptId, @RequestParam String userId, @RequestParam String userName){
