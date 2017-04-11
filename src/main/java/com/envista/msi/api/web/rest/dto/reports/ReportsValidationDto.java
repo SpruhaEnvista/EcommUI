546e2c9da3e16b4925d8a587b0db8cae171cbece
Ashish Kumar <ASingh@envistacorp.com>
2017-04-11 12:12:35 +0530

    validation dao added for validation procs

--- src/main/java/com/envista/msi/api/web/rest/dto/reports/ReportsValidationDto.java
+++ src/main/java/com/envista/msi/api/web/rest/dto/reports/ReportsValidationDto.java
@@ -13,6 +13,65 @@ import java.io.Serializable;
                         @StoredProcedureParameter(mode = ParameterMode.IN, name = "p_saved_sched_rpt_id", type = Long.class),
                         @StoredProcedureParameter(mode= ParameterMode.IN, name = "p_user_id",type = Long.class),
                         @StoredProcedureParameter(mode = ParameterMode.REF_CURSOR, name = "p_verificationMsg",type = Void.class)
+                }),
+        @NamedStoredProcedureQuery(name = "ReportsValidation.verifySavedSchedShippers", procedureName = "shp_rpt_svdsched_shipper_proc",
+        resultSetMappings = "VerifySavedSchedShippers",
+        parameters = {
+                @StoredProcedureParameter(mode = ParameterMode.IN, name = "p_saved_sched_rpt_id", type = Long.class),
+                @StoredProcedureParameter(mode= ParameterMode.IN, name = "p_user_id",type = Long.class),
+                @StoredProcedureParameter(mode = ParameterMode.REF_CURSOR, name = "p_verificationMsg",type = Void.class)
+        }),
+        @NamedStoredProcedureQuery(name = "ReportsValidation.verifyAssignedReport", procedureName = "shp_rpt_verify_assiged_rpt",
+                resultSetMappings = "VerifyAssignedReport",
+                parameters = {
+                        @StoredProcedureParameter(mode = ParameterMode.IN, name = "p_user_id", type = Long.class),
+                        @StoredProcedureParameter(mode= ParameterMode.IN, name = "p_rpt_id",type = Long.class),
+                        @StoredProcedureParameter(mode = ParameterMode.REF_CURSOR, name = "is_rpt_present",type = Void.class)
+                }),
+        @NamedStoredProcedureQuery(name = "ReportsValidation.verifySavedSchedShipperGroups", procedureName = "shp_rpt_svdsched_shipGrps_proc",
+                resultSetMappings = "VerifySavedSchedShippers",
+                parameters = {
+                        @StoredProcedureParameter(mode = ParameterMode.IN, name = "p_saved_sched_rpt_id", type = Long.class),
+                        @StoredProcedureParameter(mode= ParameterMode.IN, name = "p_user_id",type = Long.class),
+                        @StoredProcedureParameter(mode = ParameterMode.REF_CURSOR, name = "p_verificationMsg",type = Void.class)
+                }),
+
+        @NamedStoredProcedureQuery(name = "ReportsValidation.verifyShipperGroups", procedureName = "shp_rpt_verify_shipGrps_proc",
+                resultSetMappings = "VerifySavedSchedShippers",
+                parameters = {
+                        @StoredProcedureParameter(mode = ParameterMode.IN, name = "p_shipperGroupIds", type = String.class),
+                        @StoredProcedureParameter(mode= ParameterMode.IN, name = "p_user_id",type = Long.class),
+                        @StoredProcedureParameter(mode = ParameterMode.REF_CURSOR, name = "p_verificationMsg",type = Void.class)
+                }),
+        @NamedStoredProcedureQuery(name = "ReportsValidation.verifyShippers", procedureName = "shp_rpt_verify_shippers_proc",
+                resultSetMappings = "VerifySavedSchedShippers",
+                parameters = {
+                        @StoredProcedureParameter(mode = ParameterMode.IN, name = "p_shipperIds", type = String.class),
+                        @StoredProcedureParameter(mode= ParameterMode.IN, name = "p_user_id",type = Long.class),
+                        @StoredProcedureParameter(mode = ParameterMode.REF_CURSOR, name = "p_verificationMsg",type = Void.class)
+                }),
+        @NamedStoredProcedureQuery(name = "ReportsValidation.verifyAssignCarrier", procedureName = "shp_rpt_verify_Assign_Crr_proc",
+                resultSetMappings = "VerifySavedSchedShippers",
+                parameters = {
+                        @StoredProcedureParameter(mode = ParameterMode.IN, name = "p_carrierIds", type = String.class),
+                        @StoredProcedureParameter(mode= ParameterMode.IN, name = "p_user_id",type = Long.class),
+                        @StoredProcedureParameter(mode = ParameterMode.REF_CURSOR, name = "p_verificationMsg",type = Void.class)
+                }),
+        @NamedStoredProcedureQuery(name = "ReportsValidation.varifyCarrier", procedureName = "shp_rpt_verifyCarrier_proc",
+                resultSetMappings = "VerifySavedSchedShippers",
+                parameters = {
+                        @StoredProcedureParameter(mode = ParameterMode.REF_CURSOR, name = "p_verificationMsg",type = Void.class),
+                        @StoredProcedureParameter(mode= ParameterMode.IN, name = "p_userId",type = Long.class),
+                        @StoredProcedureParameter(mode= ParameterMode.IN,name = "p_rptId",type = Long.class),
+                        @StoredProcedureParameter(mode = ParameterMode.IN,name = "p_savedschedRptId",type = Long.class)
+
+                }),
+        @NamedStoredProcedureQuery(name = "ReportsValidation.verifyAssignedAccounts", procedureName = "shp_rpt_assigned_accounts_proc",
+                resultSetMappings = "VerifyAccounts",
+                parameters = {
+                        @StoredProcedureParameter(mode = ParameterMode.IN, name = "p_customerIds", type = String.class),
+                        @StoredProcedureParameter(mode= ParameterMode.IN, name = "p_user_id",type = Long.class),
+                        @StoredProcedureParameter(mode = ParameterMode.REF_CURSOR, name = "p_verificationMsg",type = Void.class)
                 })
         })
 @SqlResultSetMappings({
@@ -21,6 +80,22 @@ import java.io.Serializable;
                         targetClass = ReportsValidationDto.class,
                         columns = {
                                 @ColumnResult(name = "p_verificationMsg", type = String.class)
+                        }
+                )
+        }),
+        @SqlResultSetMapping(name = "VerifySavedSchedShippers", classes = {
+                @ConstructorResult(
+                        targetClass = ReportsValidationDto.class,
+                        columns = {
+                                @ColumnResult(name = "p_verificationMsg", type = String.class)
+                        }
+                )
+        }),
+        @SqlResultSetMapping(name = "VerifyAssignedReport", classes = {
+                @ConstructorResult(
+                        targetClass = ReportsValidationDto.class,
+                        columns = {
+                                @ColumnResult(name = "is_rpt_present", type = Long.class)
                         }
                 )
         })
@@ -34,6 +109,17 @@ public class ReportsValidationDto implements Serializable {
 
     @Column(name = "p_verificationMsg")
     private String verificationMsg;
+
+    @Column(name = "is_rpt_present")
+    private  long  isRptPresent;
+
+    public long getIsRptPresent() {
+        return isRptPresent;
+    }
+
+    public void setIsRptPresent(long isRptPresent) {
+        this.isRptPresent = isRptPresent;
+    }
 
     public ReportsValidationDto() {
     }
@@ -56,6 +142,10 @@ public class ReportsValidationDto implements Serializable {
 
     public void setVerificationMsg(String verificationMsg) {
         this.verificationMsg = verificationMsg;
+    }
+
+    public ReportsValidationDto(long isRptPresent) {
+        this.isRptPresent = isRptPresent;
     }
 }
 
