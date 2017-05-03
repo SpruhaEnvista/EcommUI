1ae442c5252a115999b35c0d9c58fa4592a3fdab
Krishna Reddy <kreddy@envistacorp.com>
2017-05-03 11:16:51 +0530

    Invoicing utility file is added.

---
+++ src/main/java/com/envista/msi/api/web/rest/util/InvoicingUtilities.java
@@ -0,0 +1,93 @@
+package com.envista.msi.api.web.rest.util;
+
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by KRISHNAREDDYM on 5/1/2017.
+ */
+public final class InvoicingUtilities {
+
+    private static Logger LOG = LoggerFactory.getLogger(InvoicingUtilities.class);
+
+
+    public static String identifyFedexCreditClass(String reaon) {
+        // TODO Auto-generated method stub
+        String creditClass;
+        if (StringUtils.containsIgnoreCase(reaon, "COMMERCIAL") || StringUtils.containsIgnoreCase(reaon, "RESI") || StringUtils.containsIgnoreCase(reaon, "BUS")
+                || StringUtils.containsIgnoreCase(reaon, "DELIVERED"))
+            creditClass = "Resi";
+        else if (StringUtils.containsIgnoreCase(reaon, "DUPLI") || StringUtils.containsIgnoreCase(reaon, "DOUBLE"))
+            creditClass = "Dup";
+        else if (StringUtils.containsIgnoreCase(reaon, "DIM"))
+            creditClass = "Dim";
+        else if (StringUtils.containsIgnoreCase(reaon, "SERVICE") || StringUtils.containsIgnoreCase(reaon, "LATE") || StringUtils.containsIgnoreCase(reaon, "GSR")
+                || StringUtils.containsIgnoreCase(reaon, "P.O") || StringUtils.containsIgnoreCase(reaon, "PROOF") || StringUtils.containsIgnoreCase(reaon, "POD"))
+            creditClass = "GSR";
+        else if (StringUtils.containsIgnoreCase(reaon, "AREA SUR") || StringUtils.containsIgnoreCase(reaon, "DEL SUR"))
+            creditClass = "DAS";
+        else if (StringUtils.containsIgnoreCase(reaon, "ADULT"))
+            creditClass = "Adult Signature";
+        else if (StringUtils.containsIgnoreCase(reaon, "DAMAGED"))
+            creditClass = "DAMAGED";
+        else if (StringUtils.containsIgnoreCase(reaon, "ADDRESS WAS COR"))
+            creditClass = "ADDY";
+        else if (StringUtils.containsIgnoreCase(reaon, "SAT"))
+            creditClass = "Saturday";
+        else if (StringUtils.containsIgnoreCase(reaon, "FUEL"))
+            creditClass = "Fuel Surcharge";
+        else if (StringUtils.containsIgnoreCase(reaon, "FRAUD"))
+            creditClass = "Third Party";
+        else if (StringUtils.containsIgnoreCase(reaon, "CLB"))
+            creditClass = "Closed Loop Billing";
+        else
+            creditClass = "Other";
+
+        return creditClass;
+    }
+
+    public static String identifyUpsCreditClass(String tranCode, String reason, String service) {
+        String creditClass = "";
+        if (StringUtils.containsIgnoreCase(tranCode, "VOD") || StringUtils.containsIgnoreCase(reason, "VOID"))
+            creditClass = "Void";
+        else if (StringUtils.containsIgnoreCase(reason, "COMMERCIAL ADDRESS") || StringUtils.containsIgnoreCase(reason, "RESIDENT"))
+            creditClass = "Resi";
+        else if (StringUtils.containsIgnoreCase(reason, "DUP") || StringUtils.containsIgnoreCase(reason, "NOT PREVIOUSLY BILLED") || StringUtils.containsIgnoreCase(reason, "NPB"))
+            creditClass = "Dup";
+        else if (StringUtils.containsIgnoreCase(reason, "DIM") || StringUtils.containsIgnoreCase(reason, "WEIGHT"))
+            creditClass = "Dim";
+        else if (StringUtils.containsIgnoreCase(reason, "REMBOURSEMENT DE SERVICE GARANTI") || StringUtils.containsIgnoreCase(tranCode, "GSR"))
+            creditClass = "GSR";
+        else if (StringUtils.containsIgnoreCase(reason, "ADDRESS") && StringUtils.containsNone(reason, "COMMERCIAL ADDRESS"))
+            creditClass = "ADDY";
+        else if (StringUtils.containsIgnoreCase(reason, "SAT"))
+            creditClass = "Saturday";
+        else if (StringUtils.containsIgnoreCase(reason, "ADDINTIAONAL HAND"))
+            creditClass = "Addintiaonal Handling";
+        else if (StringUtils.containsIgnoreCase(reason, "LARGE P"))
+            creditClass = "Large Package";
+        else if (StringUtils.containsIgnoreCase(reason, "FUEL"))
+            creditClass = "Fuel Surcharge";
+        else if (StringUtils.containsIgnoreCase(reason, "THIRD PARTY") || StringUtils.containsIgnoreCase(reason, "TP") || StringUtils.containsIgnoreCase(reason, "UNAUTHORIZED"))
+            creditClass = "Third Party";
+        else if (StringUtils.containsIgnoreCase(service, "GST") || StringUtils.containsIgnoreCase(service, "HST") || StringUtils.containsIgnoreCase(service, "VAT"))
+            creditClass = "TAX";
+        else if (StringUtils.containsIgnoreCase(service, "ZONE ADJUSTMENT"))
+            creditClass = "Zone Adjustment";
+        else if (StringUtils.containsIgnoreCase(service, "CORP REBATE"))
+            creditClass = "Corporate Rebates";
+        else if (StringUtils.containsIgnoreCase(reason, "BILLING ADJUSTMENT FOR W/E"))
+            creditClass = "Billing Adjustment";
+        else if (StringUtils.containsIgnoreCase(reason, "CLB"))
+            creditClass = "Closed Loop Billing";
+        else if (StringUtils.equals(reason, ",,,,") || StringUtils.containsIgnoreCase(reason, "DUP BILLING") || StringUtils.containsIgnoreCase(reason, "DUPLICATE SHIPMENTS")
+                || StringUtils.containsIgnoreCase(reason, "SHIPMENT NOT PREVIOUSLY BILLED") || StringUtils.containsIgnoreCase(reason, "RESIDENTIAL SERVICE ADJUSTMENT"))
+            creditClass = "Misc";
+        else
+            creditClass = "Other";
+
+        return creditClass;
+    }
+
+}
