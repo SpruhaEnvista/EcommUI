2d5d2d8a59eec1238fab31453367caa2d7f6d8d0
sreenivaslua <sreenivaslua@envistacorp.com>
2017-05-06 18:41:52 +0530

    Merge remote-tracking branch 'remotes/origin/Standard_Branch_for_UAT_Demo' into feature/MSI-28163-api-for-change-password-user-details

--- src/main/java/com/envista/msi/api/web/rest/ReportsController.java
+++ src/main/java/com/envista/msi/api/web/rest/ReportsController.java
@@ -101,7 +101,7 @@ public class ReportsController {
     }
     @RequestMapping(value = "/savedschedreports", method = {RequestMethod.GET, RequestMethod.POST, RequestMethod.OPTIONS}, produces = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<List<SavedSchedReportsDto>> getSavedSchedReports(@RequestParam String userId,@RequestParam(required = false) String folderId){
-        List<SavedSchedReportsDto> resultsList = reportsService.getSavedSchedReports(Long.parseLong(userId), (folderId == null ? 0 : Long.parseLong(folderId)));
+        List<SavedSchedReportsDto> resultsList = reportsService.getSavedSchedReports(Long.parseLong(userId), (folderId == null ? 0 : Long.parseLong(folderId.trim())));
         return new ResponseEntity<List<SavedSchedReportsDto>>(resultsList, HttpStatus.OK);
     }
     @RequestMapping(value = "/savedschedtemplates", method = {RequestMethod.GET, RequestMethod.POST, RequestMethod.OPTIONS}, produces = {MediaType.APPLICATION_JSON_VALUE})
