aad1725c6623b5c319fec7bc841d9fa1961a7fe1
Krishna Reddy <kreddy@envistacorp.com>
2017-05-09 17:25:49 +0530

    Dashboard API calls are added.

---
+++ src/main/java/com/envista/msi/api/dao/invoicing/DashBoardDao.java
@@ -0,0 +1,28 @@
+package com.envista.msi.api.dao.invoicing;
+
+import com.envista.msi.api.domain.PersistentContext;
+import com.envista.msi.api.domain.util.QueryParameter;
+import com.envista.msi.api.domain.util.StoredProcedureParameter;
+import com.envista.msi.api.web.rest.dto.invoicing.DashBoardDto;
+import org.springframework.stereotype.Repository;
+
+import javax.inject.Inject;
+import java.util.List;
+
+/**
+ * Created by KRISHNAREDDYM on 5/8/2017.
+ */
+@Repository("dashBoardDAO")
+public class DashBoardDao {
+
+    @Inject
+    private PersistentContext persistentContext;
+
+    public List<DashBoardDto> getDashBoardInfo(String fromDate, String toDate, String actionType) {
+
+        QueryParameter queryParameter = StoredProcedureParameter.with("P_FROM_DATE", fromDate)
+                .and("P_TO_DATE", toDate).and("P_ACTION_TYPE", actionType);
+
+        return persistentContext.findEntities("DashBoardDto.getDashBoardInfo", queryParameter);
+    }
+}
---
+++ src/main/java/com/envista/msi/api/web/rest/invoicing/DashBoardController.java
@@ -0,0 +1,54 @@
+package com.envista.msi.api.web.rest.invoicing;
+
+import com.envista.msi.api.service.invoicing.DashBoardService;
+import com.envista.msi.api.web.rest.dto.invoicing.DashBoardDto;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.inject.Inject;
+import java.util.List;
+
+/**
+ * Created by KRISHNAREDDYM on 5/8/2017.
+ */
+@RestController
+@RequestMapping("/api/dashBoard")
+public class DashBoardController {
+
+    private final Logger log = LoggerFactory.getLogger(DashBoardController.class);
+
+    @Inject
+    private DashBoardService service;
+
+    /**
+     * HTTP GET - Get all Voices
+     */
+    @RequestMapping(value = "/getDashBoardInfo", params = {"fromDate", "toDate"}, produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.GET)
+    public ResponseEntity<JSONObject> getDashBoardInfo(@RequestParam String fromDate, @RequestParam String toDate) throws JSONException {
+        log.info("***getDashBoardInfo method started****");
+        JSONObject jsonObject = new JSONObject();
+        List<DashBoardDto> totalsByBusinessPartner = service.getDashBoardInfo(fromDate, toDate, "totalsByBusinessPartner");
+        jsonObject.put("totalsByBusinessPartner", totalsByBusinessPartner);
+
+        List<DashBoardDto> totalsByCarrier = service.getDashBoardInfo(fromDate, toDate, "totalsByCarrier");
+        jsonObject.put("totalsByCarrier", totalsByCarrier);
+
+        List<DashBoardDto> totalsByInvCatagory = service.getDashBoardInfo(fromDate, toDate, "totalsByInvCatagory");
+        jsonObject.put("totalsByInvCatagory", totalsByInvCatagory);
+
+        List<DashBoardDto> totalsByCreditClass = service.getDashBoardInfo(fromDate, toDate, "totalsByCreditClass");
+        jsonObject.put("totalsByCreditClass", totalsByCreditClass);
+
+        log.info("***getDashBoardInfo json***==== " + jsonObject);
+        return new ResponseEntity<JSONObject>(jsonObject, HttpStatus.OK);
+    }
+}
