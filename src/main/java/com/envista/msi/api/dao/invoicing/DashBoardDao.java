f1c0f33cbbe0cf24ec80a7d27ffbd4d0bfe8f5e8
Krishna Reddy <kreddy@envistacorp.com>
2017-06-19 19:41:00 +0530

    pending count method is changed.

--- src/main/java/com/envista/msi/api/dao/invoicing/DashBoardDao.java
+++ src/main/java/com/envista/msi/api/dao/invoicing/DashBoardDao.java
@@ -38,6 +38,22 @@ public class DashBoardDao {
 
     }
 
+    public int getPendingCreditsCount(String fromDate, String toDate, String actionType) {
+
+        QueryParameter queryParameter = StoredProcedureParameter.with("P_FROM_DATE", fromDate)
+                .and("P_TO_DATE", toDate).and("P_ACTION_TYPE", actionType);
+
+        List<DashBoardDto> dtos = persistentContext.findEntities("DashBoardDto.getPendingCreditsCount", queryParameter);
+
+        int count = 0;
+
+        if (null != dtos && dtos.size() > 0) {
+            count = dtos.get(0).getPendingCreditsCount();
+        }
+
+        return count;
+    }
+
     public int closeCurrentWeekCredits(String ebillManifestIds, String action, Long weekEndId) {
 
         QueryParameter queryParameter = StoredProcedureParameter.with("P_WEEK_END_ID", weekEndId).and("P_ACTION", action)
