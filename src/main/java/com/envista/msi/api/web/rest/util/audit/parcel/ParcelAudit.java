569399c8e27a7f13a16eb7cfb5c6af384ebe0cbf
Sujit kumar <ssahoo@envistacorp.com>
2017-08-09 17:00:21 +0530

    added parcel audit main class

--- src/main/java/com/envista/msi/api/web/rest/util/audit/parcel/ParcelAudit.java
+++ src/main/java/com/envista/msi/api/web/rest/util/audit/parcel/ParcelAudit.java
@@ -1,11 +1,11 @@
-package com.envista.msi.api.rest.audit;
+package com.envista.msi.api.web.rest.util.audit.parcel;
+
 
 import com.envista.msi.api.web.rest.util.CommonUtil;
 
 /**
- * Created by Sujit kumar on 08/08/2017.
+ * Created by Sujit kumar on 09/08/2017.
  */
-
 public class ParcelAudit {
     //private static final String AUDIT_API_BASE_PATH_VALUE = "http://localhost:8086/api/parcel/rtr";
     private static final String AUDIT_API_BASE_PATH_VALUE = "https://avatar.myshipinfo.com/msiapi/api/parcel/rtr";
@@ -20,7 +20,7 @@ public class ParcelAudit {
         try{customerId = args[2].trim();}catch (Exception e){}
 
         if(fromDate != null && !fromDate.isEmpty() && toDate != null && !toDate.isEmpty()){
-            System.out.println("Start Auditing...");
+            System.out.println("Start Parcel Auditing...");
             String queryString = "fromDate=" +  fromDate + "&toDate=" + toDate + "&customerId=" + customerId;
             try {
                 String url = AUDIT_API_BASE_PATH_VALUE + "/auditParcel" + "?" + queryString;
@@ -30,5 +30,6 @@ public class ParcelAudit {
                 e.printStackTrace();
             }
         }
+        System.out.println("Done Parcel Auditing...");
     }
 }
