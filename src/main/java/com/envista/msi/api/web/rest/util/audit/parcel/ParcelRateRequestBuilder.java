6add94cec3af867029e5d211ea51d32a5857d445
Krishna Reddy <kreddy@envistacorp.com>
2018-05-07 19:21:38 +0530

    taking merge from uat demo branch
    Conflicts:
    	src/main/java/com/envista/msi/api/service/rtr/ParcelRTRService.java
    	src/main/java/com/envista/msi/api/web/rest/dto/rtr/ParcelAuditDetailsDto.java

--- src/main/java/com/envista/msi/api/web/rest/util/audit/parcel/ParcelRateRequestBuilder.java
+++ src/main/java/com/envista/msi/api/web/rest/util/audit/parcel/ParcelRateRequestBuilder.java
@@ -6,10 +6,7 @@ import com.envista.msi.api.web.rest.dto.rtr.ParcelAuditDetailsDto;
 import com.envista.msi.api.web.rest.util.DateUtil;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by Sujit kumar on 21/06/2017.
