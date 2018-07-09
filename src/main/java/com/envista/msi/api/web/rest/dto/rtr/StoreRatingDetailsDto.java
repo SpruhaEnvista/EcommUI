208bd31e8b3983adc890746afddd1ff632051a14
Ashish Kumar <ASingh@envistacorp.com>
2018-07-09 15:15:04 +0530

    import changes changes

--- src/main/java/com/envista/msi/api/web/rest/dto/rtr/StoreRatingDetailsDto.java
+++ src/main/java/com/envista/msi/api/web/rest/dto/rtr/StoreRatingDetailsDto.java
@@ -1,6 +1,12 @@
 package com.envista.msi.api.web.rest.dto.rtr;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedStoredProcedureQueries;
+import javax.persistence.NamedStoredProcedureQuery;
+import javax.persistence.ParameterMode;
+import javax.persistence.StoredProcedureParameter;
 import java.io.Serializable;
 
 
