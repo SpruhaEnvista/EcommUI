526abcb35c31e550a039f978cebb1dbce7eacea1
Ashish Kumar <ASingh@envistacorp.com>
2018-07-09 15:52:52 +0530

    import changes

--- src/main/java/com/envista/msi/api/web/rest/RatingController.java
+++ src/main/java/com/envista/msi/api/web/rest/RatingController.java
@@ -9,7 +9,11 @@ import org.json.JSONObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
 
 
 import javax.inject.Inject;
