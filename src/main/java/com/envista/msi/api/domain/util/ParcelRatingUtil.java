cb60350b4ef29cb0ce047fc4b9fa0db3ad9a614a
Krishna Reddy <kreddy@envistacorp.com>
2018-10-07 20:10:40 +0530

    substracting rated gross fue logic is added.

--- src/main/java/com/envista/msi/api/domain/util/ParcelRatingUtil.java
+++ src/main/java/com/envista/msi/api/domain/util/ParcelRatingUtil.java
@@ -1588,7 +1588,7 @@ public class ParcelRatingUtil {
                 accessorialDto.setCustFuleSurDis(accessorialDto.getCustFuleSurDis().add(dto.getCustFuleSurDis()));
                 accessorialDto.setEarnedDis(accessorialDto.getEarnedDis().add(dto.getEarnedDis()));
                 accessorialDto.setFuleSurDis(accessorialDto.getFuleSurDis().add(dto.getFuleSurDis()));
-                accessorialDto.setRatedGrossFuel(accessorialDto.getRatedGrossFuel());
+                accessorialDto.setRatedGrossFuel(accessorialDto.getRatedGrossFuel().add(dto.getRatedGrossFuel()));
             }
 
         }
